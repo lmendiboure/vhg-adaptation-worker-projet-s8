57b5f110ece42188f70a0b5f30ac465c886c3bc6
lmendiboure <leomendi14@hotmail.fr>
2016-04-04 12:22:40 +0200

    Update context.py

--- adaptation/context.py
+++ adaptation/context.py
@@ -31,35 +31,35 @@ def get_plus_dash_folder(context,number):
 def get_dash_folder(context,number):
     return os.path.join(context["folder_out"], "dash_description_"+str(number))
 
-def get_zip_folder(context,number):
+def get_description_zip_folder(context,number):
     return os.path.join(context["folder_out"], "zip_dash_description_"+str(number)+".zip")
 
-def get_plus_zip_folder(context,number):
+def get_description_plus_zip_folder(context,number):
     return os.path.join(context["folder_out"], "zip_plus_dash_description_"+str(number)+".zip")
+
+def get_mpd_zip_folder(context):
+    return os.path.join(context["folder_out"],context["videoID"]+".zip")
 
 def get_dash_mpd_file_folder(context):
     return os.path.join(context["folder_out"],"mpd_folder")
 
-def get_plus_dash_mpd_file_folder(context):
-    return os.path.join(context["folder_out"],"mpd_folder_plus")
-
 def get_dash_mpd_file_path(context,number):
-    return os.path.join(get_dash_folder(context,number), context["videoID"]+str(number) + ".mpd")
+    return os.path.join(get_dash_folder(context,number), str(number) + ".mpd")
 
 def get_plus_dash_mpd_file_path(context,number):
-    return os.path.join(get_plus_dash_folder(context,number), context["videoID"]+"_plus_"+str(number) + ".mpd")
+    return os.path.join(get_plus_dash_folder(context,number), "plus_"+str(number) + ".mpd")
 
 # Need to be modified
 def get_postProcessor_path(context):
-    return os.path.join("~/Documents/Projet/MD-openh264-dev-projetS8/","postProcessor")
+    return os.path.abspath("adaptation/MD-openh264/postProcessor ")
 
 # Need to be modified
 def get_welsenc_path(context):
-    return os.path.join("~/Documents/Projet/openh264-master/h264enc ~/Documents/Projet/openh264-master/mytests/","welsenc.cfg")
+    return os.path.join(os.path.abspath("adaptation/openh264-projetS8/h264enc ") + os.path.abspath("adaptation/openh264-projetS8/mytests/welsenc.cfg"))
 
 # Need to be modified
 def get_layer_path(context):
-    return os.path.join("home/utilisateur/Documents/Projet/openh264-master/mytests/","layer2.cfg")
+    return os.path.abspath("adaptation/openh264-projetS8/mytests/layer2.cfg")
 
 def get_dim_as_str(context):
     return str(context["target_width"]) + "x" + str(context["target_height"])
