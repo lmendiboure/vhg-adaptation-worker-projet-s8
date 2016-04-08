de3b1555add6ba31a4e88f465e331f6dab8d8e2e
lmendiboure <leomendi14@hotmail.fr>
2016-04-08 16:54:47 +0200

    Update settings.py

--- adaptation/settings.py
+++ adaptation/settings.py
@@ -3,3 +3,4 @@ CELERY_RESULT_BACKEND = 'amqp://guest:guest@localhost'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_ACCEPT_CONTENT = ['json']
+config = {"folder_out": "/"}
