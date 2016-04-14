c6313428a083111a36be55973dbe6ed23b0e6db3
lmendiboure <leomendi14@hotmail.fr>
2016-04-14 17:45:28 +0200

    Update deploy.sh
    Last version of the deployment

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,20 @@
 #Script for Ubuntu 14.04
 
 #Install RabbitMQ for Celery, Then Celery and MediaInfo wrapper for python
-apt-get -y install rabbitmq-server python-pip mediainfo python-lxml python-dev wget yasm nasm
-pip install celery pymediainfo
+sudo apt-get -y install rabbitmq-server python-pip mediainfo python-lxml python-dev wget yasm nasm
+sudo pip install celery pymediainfo
 
 #Compile Manually MP4Box from GPAC project
-./compile_MP4Box.sh
+chmod 777 compile_MP4Box.sh
+sudo ./compile_MP4Box.sh
 
 #Compile FFmpeg
-./compile_ffmpeg.sh
+chmod 777 compile_ffmpeg.sh
+sudo ./compile_ffmpeg.sh
 
+
+chmod 777 start.sh
+chmod 777 MyCommand.txt
 
 # Download and compile OpenH264 to use h264enc (encoding the videos with openh264)
 
